3f0d5353c8acf3d5f6e65df7549410fce5aa18db
vadim2071 <vadim207138@gmail.com>
2021-04-26 00:52:46 +0300

    practical task 01

---
+++ task_01/Task_01/Task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            var UserName = Console.ReadLine();
+            Console.WriteLine($"Привет, {UserName}, сегодня {DateTime.Today.ToLongDateString()}");
+        }
+    }
+}
