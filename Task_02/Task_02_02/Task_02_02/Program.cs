8bc2f332ff61f5e32c2f7ae457f8b7c01588995a
IVG <vadim207138@gmail.com>
2021-04-27 01:54:17 +0300

    Begin Task 4

---
+++ Task_02/Task_02_02/Task_02_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_02_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ProductName01 = "Лампочка 19 Вт";
+            string ProductName02 = "Выключатель";
+            string ProductName03 = "Розетка";
+            int ProductNumber01 = 5;
+            int ProductNumber02 = 5;
+            int ProductNumber03 = 2;
+            float ProdustPrice01 = 159.9;
+            float ProdustPrice02 = 90.9;
+            float ProdustPrice03 = 201.5;
+
+
+
+            Console.WriteLine($"1. {ProductName01} {ProductNumber01} {ProdustPrice01} {ProductNumber01 * ProdustPrice01}");
+            Console.WriteLine($"2. {ProductName02} {ProductNumber02} {ProdustPrice02} {ProductNumber02 * ProdustPrice02}");
+            Console.WriteLine($"3. {ProductName03} {ProductNumber03} {ProdustPrice03} {ProductNumber03 * ProdustPrice03}");
+        }
+    }
+}
