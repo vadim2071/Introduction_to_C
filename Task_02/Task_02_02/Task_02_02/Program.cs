7c3c62efec5ff76acabbaa4352f1cec20e90470c
IVG <vadim207138@gmail.com>
2021-04-28 00:21:29 +0300

    Make task 4

--- Task_02/Task_02_02/Task_02_02/Program.cs
+++ Task_02/Task_02_02/Task_02_02/Program.cs
@@ -12,12 +12,12 @@ namespace Task_02_02
             int ProductNumber01 = 5;
             int ProductNumber02 = 5;
             int ProductNumber03 = 2;
-            float ProdustPrice01 = 159.9;
-            float ProdustPrice02 = 90.9;
-            float ProdustPrice03 = 201.5;
+            double ProdustPrice01 = 159.9;
+            double ProdustPrice02 = 90.9;
+            double ProdustPrice03 = 201.5;
 
 
-
+            Console.WriteLine(DateTime.Today.ToLongDateString());
             Console.WriteLine($"1. {ProductName01} {ProductNumber01} {ProdustPrice01} {ProductNumber01 * ProdustPrice01}");
             Console.WriteLine($"2. {ProductName02} {ProductNumber02} {ProdustPrice02} {ProductNumber02 * ProdustPrice02}");
             Console.WriteLine($"3. {ProductName03} {ProductNumber03} {ProdustPrice03} {ProductNumber03 * ProdustPrice03}");
