13b13b960a7ae8a54af6b5f7be9ae2e5b1f839f2
Vadim2071 <vadim207138@gmail.com>
2021-04-28 13:56:54 +0300

    End make task 4

--- Task_02/Task_02_02/Task_02_02/Program.cs
+++ Task_02/Task_02_02/Task_02_02/Program.cs
@@ -9,18 +9,26 @@ namespace Task_02_02
             string ProductName01 = "Лампочка 19 Вт";
             string ProductName02 = "Выключатель";
             string ProductName03 = "Розетка";
-            int ProductNumber01 = 5;
-            int ProductNumber02 = 5;
-            int ProductNumber03 = 2;
-            double ProdustPrice01 = 159.9;
-            double ProdustPrice02 = 90.9;
-            double ProdustPrice03 = 201.5;
+            int ProductQuantity01 = 5;
+            int ProductQuantity02 = 5;
+            int ProductQuantity03 = 2;
+            double ProductPrice01 = 159.95;
+            double ProductPrice02 = 90.95;
+            double ProductPrice03 = 201.54;
 
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("ИНН 2374569812437");
+            Console.WriteLine("ООО Купите у нас все \n");
 
-            Console.WriteLine(DateTime.Today.ToLongDateString());
-            Console.WriteLine($"1. {ProductName01} {ProductNumber01} {ProdustPrice01} {ProductNumber01 * ProdustPrice01}");
-            Console.WriteLine($"2. {ProductName02} {ProductNumber02} {ProdustPrice02} {ProductNumber02 * ProdustPrice02}");
-            Console.WriteLine($"3. {ProductName03} {ProductNumber03} {ProdustPrice03} {ProductNumber03 * ProdustPrice03}");
+            Console.WriteLine("   {0, -18} {1, 7} {2, -5} {3, 6}", "Наименование ", "цена", "кол-во", "Сумма");
+
+            Console.WriteLine("1. {0, -18} {1, 7} {2, 4} {3, 9}", ProductName01, ProductPrice01, ProductQuantity01, ProductQuantity01 * ProductPrice01);
+            Console.WriteLine("2. {0, -18} {1, 7} {2, 4} {3, 9}", ProductName02, ProductPrice02, ProductQuantity02, ProductQuantity02 * ProductPrice02);
+            Console.WriteLine("3. {0, -18} {1, 7} {2, 4} {3, 9}", ProductName03, ProductPrice03, ProductQuantity03, ProductQuantity03 * ProductPrice03);
+
+            Console.WriteLine("--------------");
+            Console.WriteLine("Итого {0,38} \n", (ProductQuantity01 * ProductPrice01) + (ProductQuantity02 * ProductPrice02) + (ProductQuantity03 * ProductPrice03));
+            Console.WriteLine("СПАСИБО");
         }
     }
 }
