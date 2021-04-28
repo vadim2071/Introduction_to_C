23564e9bfc849c0e59400d6bf38ad908aa794842
Vadim2071 <vadim207138@gmail.com>
2021-04-28 17:58:37 +0300

    Begin task 6

---
+++ Task_02/Task_6/Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirmFirst = 0b1111100; //дни  работы Фирма 1
+            int FirmSecond = 0b0111110; //дни  работы Фирма 2
+            int FirmThird = 0b1111111; //дни  работы Фирма 3
+
+        }
+    }
+}
