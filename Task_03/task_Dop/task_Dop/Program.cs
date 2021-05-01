73021de084e9571eee0d6a45e97041a514d05ad2
vadim2071 <vadim207138@gmail.com>
2021-05-02 02:17:37 +0300

    task dop Begin

---
+++ Task_03/task_Dop/task_Dop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task_Dop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] Battlefield = new char[10,10];
+            string Stroke = "";
+
+            //заполняем первоначальными значениями "O"
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    Battlefield[x, y] = 'O';
+                }
+            }
+
+            //Генерация кораблей
+
+            //вывод содержимого массива на экран
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    Stroke = Stroke + Battlefield[x, y];
+                }
+                Console.WriteLine(Stroke);
+                Stroke = "";
+            }
+
+        }
+    }
+}
