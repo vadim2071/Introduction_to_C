75996b9bb29a1eed3e04a0c415913ed8d04fc4af
Vadim Ivanov <vadim207138@gmail.com>
2021-05-06 23:37:52 +0300

    task 1 continue

--- Task_04/task1/task1/Program.cs
+++ Task_04/task1/task1/Program.cs
@@ -6,7 +6,17 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+             принимающий на вход ФИО в разных аргументах и возвращающий объединённую 
+             строку с ФИО. Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+             Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+             принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. 
+             Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.*/
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+
         }
     }
 }
