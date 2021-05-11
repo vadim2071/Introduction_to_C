9422075e327f2e0968ab55080a61baffd5118777
IVG <vadim207138@gmail.com>
2021-05-12 01:03:15 +0300

    task 5 v.0.1

--- Task_05/task5/task5/Program.cs
+++ Task_05/task5/task5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace task5
 {
@@ -6,7 +7,7 @@ namespace task5
     {
         /* Список задач (ToDo-list):
         - написать приложение для ввода списка задач;
-        - задачу описать классом ToDo с полями Title и IsDone;
+        + задачу описать классом ToDo с полями Title и IsDone;
         - на старте, если есть файл tasks.json/xml/bin (выбрать формат), загрузить из него массив имеющихся задач и вывести их на экран;
         - если задача выполнена, вывести перед её названием строку «[x]»;
         - вывести порядковый номер для каждой задачи;
@@ -14,7 +15,16 @@ namespace task5
         - записать актуальный массив задач в файл tasks.json/xml/bin.*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string filename = "tasks.json"; //имя файла для записи
+            //переменная для хранения пути до файла на рабочем столе
+            string FilePass = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+
+            if (File.Exists(FilePass + filename))
+            {
+                
+            }
+
+            //ToDo List1 = new ToDo();
         }
     }
 }
