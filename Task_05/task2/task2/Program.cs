6c08d1ce60e3817eb9a9c5db5261aab6c497de77
IVG <vadim207138@gmail.com>
2021-05-11 22:52:43 +0300

    task 2 end

---
+++ Task_05/task2/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+//Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "task2.txt"; //имя файла для записи
+            string time;
+            //переменная для хранения пути до файла на рабочем столе
+            string FilePass = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+            time = DateTime.Now.ToLongTimeString();
+
+            Console.WriteLine($"в файл {filename} сейчас запишем текущее время {time}");
+            if (File.Exists(FilePass+filename))
+            {
+                File.AppendAllText(FilePass + filename, Environment.NewLine);
+                File.AppendAllText(FilePass + filename, time);
+            }
+            else
+            {
+                File.WriteAllText(FilePass + filename, time);
+            };
+            
+        }
+    }
+}
