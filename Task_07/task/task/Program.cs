4d69087bd767183f84f1f974ff5c11ff2280da2a
IVG <vadim207138@gmail.com>
2021-05-19 00:12:18 +0300

    End all task

---
+++ Task_07/task/task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter you name");
+            string name = Console.ReadLine();
+            Console.WriteLine("How many age");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}! You are {age}");
+            if (Convert.ToInt32(age) > 65)
+            {
+                Console.WriteLine("you pensioner?");
+            }
+            Console.WriteLine($"Do 100 let ostalos {100 - Convert.ToInt32(age)} let");
+        }
+    }
+}
